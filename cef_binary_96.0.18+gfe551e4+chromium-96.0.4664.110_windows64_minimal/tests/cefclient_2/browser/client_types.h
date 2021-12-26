ba3e4abcea61413643b2a6f4a64eb01f08291c05
PioneerRedwood <chrisredwood123@gmail.com>
2021-12-27 00:00:37 +0900

    cefclient_2 added

---
+++ cef_binary_96.0.18+gfe551e4+chromium-96.0.4664.110_windows64_minimal/tests/cefclient_2/browser/client_types.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2015 The Chromium Embedded Framework Authors. All rights
+// reserved. Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file.
+
+#ifndef CEF_TESTS_CEFCLIENT_BROWSER_CLIENT_TYPES_H_
+#define CEF_TESTS_CEFCLIENT_BROWSER_CLIENT_TYPES_H_
+#pragma once
+
+#include "include/cef_base.h"
+
+#define ClientWindowHandle CefWindowHandle
+
+#endif  // CEF_TESTS_CEFCLIENT_BROWSER_CLIENT_TYPES_H_
